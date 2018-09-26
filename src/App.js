2b31a80a6e9b50cda93e29c5bff9c0d63612844c
Mads Roloff <Mads@roloff-design.dk>
2018-09-26 16:11:34 +0200

    Navbar Structure & styled

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import logo from './logo.svg';
-import './App.css';
-import Navbarcontainer from './containers/navBar/index';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./App.css";
+import Navbarcontainer from "./containers/navBar/index";
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
       <div className="App">
-       <Navbarcontainer />
+        <Navbarcontainer />
       </div>
     );
   }
 }
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
 export default App;
