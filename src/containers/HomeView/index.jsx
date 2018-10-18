bc1c0796c0c6014074e5f8c9857c046272289633
Roloffqt <mads@roloff-design.dk>
2018-10-18 15:00:04 +0200

    Placement progress

--- src/containers/HomeView/index.jsx
+++ src/containers/HomeView/index.jsx
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
 import testimg1 from './testimg1.jpg';
 
 class HomeViewIndex extends Component {
@@ -20,16 +13,16 @@ class HomeViewIndex extends Component {
     const { classes } = this.props;
     return (
       <div className={classes.root}>
-      <div className={classes.img}>
-        <img className={classes.img_size} src={testimg1} />
+      <div className={classes.teaserbar}>
       </div>
-      <div className={classes.img}>
-        <img className={classes.img_size} src={testimg1} />
+      <div className={classes.teaserbar}>
       </div>
-      <div className={classes.img}>
-        <img className={classes.img_size} src={testimg1} />
+      <div className={classes.teaserbar}>
       </div>
-      <h1>hej</h1>
+      <div className={classes.teaserbar}>
+      </div>
+      <div className={classes.teaserbar}>
+      </div>
       </div>
     );
   }
@@ -41,17 +34,20 @@ HomeViewIndex.propTypes = {
 
 const styles = () => ({
   root: {
-    marginTop: '100px',
+    margin: '100px auto',
     display :'flex',
     flexDirection: 'row',
+    transform: 'rotate(2deg)',
+    width: '960px',
+    maxHeight: '100vh',
   },
-  img: {
+  teaserbar: {
     display: 'flex',
-  },
-  img_size:{
-    maxWidth: '100%',
-    height: '60vh',
+    backgroundImage: `url('${testimg1}')`,
+    backgroundPosition: 'center center',
     width: '100%',
+    height: '75vh',
+    margin: '4px',
   }
 });
 
